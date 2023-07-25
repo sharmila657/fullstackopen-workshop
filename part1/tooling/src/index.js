2060d1a7b44a2387ad56d836f06a54ffe340441a
sharmila657 <sharmilajirel8@gmail.com>
2023-07-25 18:12:07 +0545

    feat : using jsx

--- part1/tooling/src/index.js
+++ part1/tooling/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+// import ReactDOM from "react-dom";
 import App from "./App";
-// import { createRoot } from "react-dom/client";
 
   let container = document.getElementById("root");
-  let root = ReactDOM.createRoot(container);
+  let root = createRoot(container);
   
   root.render(React.createElement(App));
--- part1/tooling/src/nameDisplay.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-let nameDisplay = (props) => {
-    return React.createElement("h1", { id: "myId" }, `Hello! ${props.firstName}`);
-};
-  
-export default nameDisplay;
