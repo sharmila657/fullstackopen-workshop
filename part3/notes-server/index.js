4bff48f1de6ecb78e5d4321b79f35660ee6c35ac
sharmila657 <sharmilajirel8@gmail.com>
2023-08-07 15:28:30 +0545

    part3-d: errorHandler

--- part3/notes-server/index.js
+++ part3/notes-server/index.js
@@ -1,7 +1,3 @@
-// console.log("Hello there");
-
-// const http = require("http")
-
 const express = require("express")
 const cors = require("cors")
 const app = express()
@@ -17,7 +13,11 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
     important: Boolean,
 });
 noteSchema.set('toJSON', {
@@ -31,35 +31,6 @@ noteSchema.set('toJSON', {
 const Note = mongoose.model('Note', noteSchema)
 
 
-
-// const requestLogger = (request, response, next) => {
-//     console.log('Method:', request.method)
-//     console.log('Path:  ', request.path)
-//     console.log('Body:  ', request.body)
-//     console.log('---')
-//     next()
-// }
-// app.use(requestLogger);
-
-// let notes = [
-//     {
-//         id: 1,
-//         content: "HTML is easy",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only JavaScript",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         important: true
-//     }];
-
-// let note 
-
 app.get('/', (request, response) => {
     response.send("<h1>Hello World</h1>")
 })
@@ -68,8 +39,6 @@ app.get("/api/notes/", (request, response) => {
     Note.find({}).then((result) => {
         response.json(result);
     });
-    // console.dir(request)
-    // return response.setHeader('content-type', 'application/json').send('{"name":"sharmila"}').end()
 })
 
 
@@ -83,12 +52,6 @@ app.get("/api/notes/:hello", (request, response) => {
         response.json(result);
     })
 
-    // if (myNote) {
-    //     response.json(myNote)
-    // }
-    // else {
-    //     response.status(404).send(`There are no notes at ${myId}`);
-    // }
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -97,50 +60,51 @@ app.delete("/api/notes/:id", (request, response) => {
         response.json(result);
     });
 
-    // notes = notes.filter(note => note.id !== myId);
-    // response.status(204).send(`The note at id ${myId} has been deleted`);
-
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
+    debugger
     const myNewPost = request.body;
-    const note = new Note(myNewPost)
-    note.save().then(result => {
+    console.log(request, "rqst");
+    
+    const note = new Note({
+        content: myNewPost.content,
+        important:myNewPost.important,
+    })
+
+    note.save()
+        .then(result => {
         response.json(result)
-    })
-        .catch(e => {
-            console.log(e)
-            response.status(500).json({error:"error is detected"})
-    })
+        })
+
+    .catch(error => next(error))
+   
+});
+
+app.put('/api/notes/:id', (request, response, next) => {
+const body = request.body
+      const note = {content:body.content,important:body.important}
+    Note.findByIdAndUpdate(
+        request.params.id, note,
+        {  new: true, runValidators: true}
+    )
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+});
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
   
-});
-
-
-app.put("/api/notes/:id", (request, response) => {
-    const myId = Number(request.params.id);
-    const updatedNote = request.body;
-    notes = notes.map((element) => {
-        if (element.id === myId) {
-            return updatedNote
-        }
-        else {
-            return element;
-        }
-    });
-
-
-    response.status(202).send(`The note at id ${myId} has been updated`);
-
-});
-
-
-
-
-// const app = http.createServer((request, response) => {
-//     response.writeHead(200, { "Content-Type":"text/json" })
-//     // response.end("<h1>Hello World</h1>")
-//     response.end(JSON.stringify(notes));
-// })
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError')
+    { return response.status(400).json({ error: error.message }) }
+  
+    next(error)
+  }
+app.use(errorHandler);
 
 const PORT = 3001;
 app.listen(PORT)
