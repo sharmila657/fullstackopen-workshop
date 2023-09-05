d4d2c046e84fd00382315a83fb965278d47d457a
sharmila675 <sharmilajirel8@gmail.com>
2023-09-05 14:43:02 +0545

    feat:part5-d changes

--- part2/notes-app/cypress/e2e/notes_app.cy.js
+++ part2/notes-app/cypress/e2e/notes_app.cy.js
@@ -1,7 +1,15 @@
 describe("Note app", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
     cy.visit("http://localhost:5173");
+  });
+  it("front page can be opened", function () {
     cy.contains("Notes");
     cy.contains("a new note");
+  });
+  it("login form can be opened", function () {
+    cy.contains("login").click();
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    
   });
 });
