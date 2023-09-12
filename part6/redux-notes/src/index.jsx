74be297f312091b31fdb61330f96236514e3d52f
sharmila675 <sharmilajirel8@gmail.com>
2023-09-12 16:02:51 +0545

    codes working upto here

--- part6/redux-notes/src/index.jsx
+++ part6/redux-notes/src/index.jsx
@@ -3,21 +3,39 @@ import {createStore} from "redux"
 import noteReducer from "./reducers/noteReducer";
 
 const store = createStore(noteReducer);
-store.dispatch({
-    type:'NEW_NOTE',
-    payload:{
-    content:'the app state is in redux store',
-    important:true,
-    id:1,
-}
-})
 
 const App = () => {
+
+const addNote = (event) =>{
+  event.preventDefault();
+  console.dir(event.target.myInput.value)
+  store.dispatch({
+    type:"NEW_NOTE",
+    payload:{
+      content:event.target.myInput.value,
+      important:true,
+      id:store.getState().length + 1,
+    }
+  })
+}
+
+  const toggleImportant = (id) =>{
+    store.dispatch({
+      type:"TOGGLE_IMORTANCE",
+      payload:{
+        id,
+      }
+    })
+  }  
     return(
       <div>
+        <form onSubmit = {addNote}>
+          <input name = "myInput"/>
+          <button>Add Note</button>
+        </form>
         <ul>
           {store.getState().map(note=>
-            <li key={note.id}>
+            <li key={note.id} onClick = {()=> toggleImportant(note.id)}>
               {note.content} <strong>{note.important ? 'important' : ''}</strong>
             </li>
           )}
@@ -29,21 +47,22 @@ const App = () => {
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+root.render(<App/>)
+store.subscribe(()=>{
+  root.render(<App/>);
+})
 
     
-    store.dispatch({
-        type:"NEW_NOTE",
-        payload:{
-            content:"the app state is in redux store",
-            important:true,
+store.dispatch({
+  type:"NEW_NOTE",
+  payload:{
+    content:"the app state is in redux store",
+    important:true,
+    id:1,
             
-        }
+}
         
-    })
+})
     
-    root.render(<App/>)
-    // store.subscribe(()=>{
-    //     return <App/>
-    // })
 
 
