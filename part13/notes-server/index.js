7c6940c402ae6094e538b27b6059b8beaec2a025
sharmila675 <sharmilajirel8@gmail.com>
2023-09-25 15:26:27 +0545

    part13-video4 complete workshop

--- part13/notes-server/index.js
+++ part13/notes-server/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
-const { Sequelize, QueryTypes } = require("sequelize");
+// const { Sequelize, QueryTypes } = require("sequelize");
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialectOptions: {
@@ -13,12 +16,67 @@ const sequelize = new Sequelize(process.env.DB_URL, {
   },
 });
 
+class Note extends Model {}
+Note.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    important: {
+      type: DataTypes.BOOLEAN,
+    },
+    date: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "note",
+  }
+);
+
+Note.sync();
+
 app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
-  });
+  const notes = await Note.findAll();
   res.json(notes);
 });
+
+app.post("/api/notes", async (req, res) => {
+  console.log(req.body);
+  const note = await Note.create(req.body);
+  res.json(note);
+});
+
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  console.log(JSON.stringify(note, null, 2));
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).send("no data found");
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
