69619bff8e86aae47072dfca7a43f96dce42d888
sharmila675 <sharmilajirel8@gmail.com>
2023-10-05 14:50:05 +0545

    part9-c video2 completed

---
+++ part9/flight_diary/src/services/diaryService.ts
@@ -0,0 +1,27 @@
+import diaryData from '../../data/entries';
+import { DiaryEntry , NonSensitiveDiaryEntry} from '../types';
+
+const diaries: DiaryEntry[] = diaryData;
+
+const getEntries = () => {
+  return diaryData;
+};
+
+const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+  getNonSensitiveEntries,
+};
